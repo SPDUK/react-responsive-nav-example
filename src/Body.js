2b172582d463d64185421f674967af1aefdf6018
SPDUK <spdevuk@gmail.com>
2019-07-09 14:58:59 +0100

    initial commit

---
+++ src/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import backgroundImage from "./images/background.png";
+
+
+const Body = () => {
+  return (
+    <div>
+      <img className="background" src={backgroundImage} alt="background" />
+    </div>
+  )
+}
+
+export default Body
